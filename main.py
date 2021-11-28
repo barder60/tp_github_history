ecc4592f817fb0c8ab36f6f4e443b2638d633d28
MasatakaIshii <masaradio78@gmail.com>
2021-11-28 13:43:44 +0100

    Exo 3 done

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from datetime import date
-
 from pyspark.ml.feature import Tokenizer
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import collect_list, desc, col, current_date, add_months, unix_timestamp
+from pyspark.sql.functions import desc, col, add_months, unix_timestamp, current_timestamp, \
+    when
 
 spark = SparkSession \
     .builder \
@@ -13,55 +12,72 @@ spark = SparkSession \
 mnm_file = "data/full.csv"
 
 df = spark.read.format("csv") \
-    .option("header", "true") \
-    .option("inferSchema", "true") \
+    .option("wholeFile", True) \
+    .option("multiline", True) \
+    .option("header", True) \
+    .option("inferSchema", True) \
     .load(mnm_file)
 
+
 def exo1():
-    print("| exo 1 |")
+    # print("| exo 1 |")
     # tokenizer = Tokenizer(inputCol="message", outputCol="tokens")
-    # tokens = tokenizer.transform(mnm_df)
+    # tokens = tokenizer.transform(df)
     # tokens.show(truncate=False)
 
-    # mnm_df.select("message").filter("message is not NULL").show(truncate=False)
+    df.select("message").filter("message is not NULL").show(truncate=False)
 
-    # df.groupby("repo").count().filter("repo is not NULL").sort(desc("count")).limit(10).show(truncate=False)
+    df.groupby("repo").count().filter("repo is not NULL").sort(desc("count")).limit(10).show(truncate=False)
+    return input(
+        "Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
 
-    return input("Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
 
 def exo2():
     print("| exo 2 |")
 
+    df.groupby("repo", "author").count().filter("repo == 'apache/spark'").sort(desc("count")).limit(1).show(
+        truncate=False)
 
-    # df.groupby("repo", "author").count().filter("repo == 'apache/spark'").sort(desc("count")).limit(1).show(truncate=False)
+    return input(
+        "Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
 
-    return input("Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
 
 def exo3():
     # La request :
     print("| exo 3 |")
 
-    today = date.today()
-    # print("Today's date:", today)
-    # current_timestamp
-    # df.show(truncate=False)
-    # df.groupby("repo", "author", "date")\
-    #     .count()\
-    #     .filter("repo == 'apache/spark'")\
-    #     .sort(unix_timestamp(col("date"), "MMM d hh:mm:ss yyyy Z").cast("timestamp")).show(truncate=False)
+    len_of_date_format = len("EEE MMM dd HH:mm:ss YYYY") - 4
+    last_month_to_search = 12
+    date_to_compare = add_months(current_timestamp(), -last_month_to_search).cast("timestamp").cast("long")
 
-    df.select("repo", "author", "date")\
-        .where(col("repo") == "apache/spark")\
-        .sort(desc(unix_timestamp(col("date"), "MMM d hh:mm:ss yyyy Z").cast("timestamp"))).show(truncate=False)
+    df.select("repo", "author", "date") \
+        .where(col("repo") == "apache/spark") \
+        .withColumn(
+        "substr_date",
+        col("date").substr(5, len_of_date_format)
+    ) \
+        .withColumn(
+        "trim_date", when(unix_timestamp(col("substr_date"), "MMM d HH:mm:ss yyyy ").isNotNull(),
+                          unix_timestamp(col("substr_date"), "MMM d HH:mm:ss yyyy "))
+            .when(unix_timestamp(col("substr_date"), "MMM dd HH:mm:ss yyyy").isNotNull(),
+                  unix_timestamp(col("substr_date"), "MMM dd HH:mm:ss yyyy"))
+            .otherwise(None)
+    ) \
+        .where(col("trim_date").isNotNull()) \
+        .where((col("trim_date") >= date_to_compare)) \
+        .groupby("repo", "author").count() \
+        .sort(desc(col("count"))) \
+        .show(n=50, truncate=40)
 
-    return input("Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
+    return input(
+        "Souhaitez-vous avancer à l excerice suivant ? (0 pour exit | n importe quelle touche pour continuer) : ")
+
 
 def exo4():
     print("| exo 4 |")
 
-
-
     return
+
 
 def exos():
     if exo1() == 0:
@@ -79,6 +95,6 @@ def exos():
     input("input de fin")
     return
 
+
 if __name__ == '__main__':
     exos()
-
